de35d5594dde6395e941753b142d05817cf6a9be
Luis Masuelli <luis@datagran.io>
2023-05-20 18:17:25 -0500

    Updating version 1.1.2 on each (they rely on Django>=4.0, but do not force 4.2).

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='django-trackmodels-xls-ritual',
-    version='1.1.1',
+    version='1.1.2',
     packages=['grimoire.django.tracked_xls'],
     url='https://github.com/luismasuelli/django-trackmodels-xls-ritual',
     license='LGPL',
     author='Luis y Anita',
     author_email='luismasuelli@hotmail.com',
     description='XLSX report plugin for django-trackmodels-ritual',
-    install_requires=['Django>=4.2', 'XlsxWriter>=0.8.7', 'django-trackmodels-ritual>=1.0.0']
+    install_requires=['Django>=4.0', 'XlsxWriter>=0.8.7', 'django-trackmodels-ritual>=1.1.2']
 )
