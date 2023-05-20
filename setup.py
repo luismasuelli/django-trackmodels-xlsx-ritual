428b10dbc7162a98de18d487522d97f24b753d20
Luis Masuelli <luis@datagran.io>
2023-05-19 23:05:10 -0500

    Version 1.0.1 - uses Django 4.2.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='django-trackmodels-xls-ritual',
-    version='1.0.0',
+    version='1.0.1',
     packages=['grimoire.django.tracked_xls'],
     url='https://github.com/luismasuelli/django-trackmodels-xls-ritual',
     license='LGPL',
     author='Luis y Anita',
     author_email='luismasuelli@hotmail.com',
     description='XLSX report plugin for django-trackmodels-ritual',
-    install_requires=['Django>=2.2', 'XlsxWriter>=0.8.7', 'django-trackmodels-ritual>=1.0.0']
+    install_requires=['Django>=4.2', 'XlsxWriter>=0.8.7', 'django-trackmodels-ritual>=1.0.0']
 )
